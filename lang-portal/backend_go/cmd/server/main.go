5b99fabcc3379e24c2d1bdfaa5bca722568d3241
tejas-2232 <tejasbachhav98@gmail.com>
2025-04-10 00:02:03 -0400

    file update with dbdir

--- lang-portal/backend_go/cmd/server/main.go
+++ lang-portal/backend_go/cmd/server/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+	"os"
+	"path/filepath"
+
 	"lang-portal-backend/internal/handlers"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +13,21 @@ import (
 )
 
 func main() {
+
+	//create database path from environment variable or use  default
+	dbPath := os.Getenv("DB_PATH")
+
+	if dbPath == "" {
+		dbPath == filepath.Join(".", "words.db")
+	}
+
+	dbDir := filepath.Dir(dbPath)
+
+	if err := os.MkdirAll(dbDir, 0755); err != nil {
+		log.Fatal("Failed to create database directory", err)
+	}
+
+	//create gin router
 	r := gin.Default()
 
 	// Setup routes
